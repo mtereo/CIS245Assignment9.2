a4609fcca86ba34e99a9ee981196c8f678d87d38
mtereo <matthew.allen.theriault.1996@gmail.com>
2023-10-26 02:28:07 +0000

    fixed indentations on lines 20-24, print statement on line 42, and capitalization on function call on line 46

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ import random
 import time
 
 def displayIntro():
-	print('''You are in a land full of dragons. In front of you,
+  print('''You are in a land full of dragons. In front of you,
 	you see two caves. In one cave, the dragon is friendly
 	and will share his treasure with you. The other dragon
 	is greedy and hungry, and will eat you on sight.''')
-	print()
+  print()
 
 def chooseCave():
     cave = ''
-        while cave != '1' and cave != '2':
-            print('Which cave will you go into? (1 or 2)')
-            cave = input()
+    while cave != '1' and cave != '2':
+      print('Which cave will you go into? (1 or 2)')
+      cave = input()
 
-    return caves
+    return cave
 
 def checkCave(chosenCave):
     print('You approach the cave...')
@@ -39,11 +39,11 @@ def checkCave(chosenCave):
     if chosenCave == str(friendlyCave):
         print('Gives you his treasure!')
     else:
-        print 'Gobbles you down in one bite!'
+        print('Gobbles you down in one bite!')
 
 
 displayIntro()
-caveNumber = choosecave()
+caveNumber = chooseCave()
 checkCave(caveNumber)
     
 print("Thanks for planing")
